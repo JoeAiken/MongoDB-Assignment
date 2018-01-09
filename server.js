7288b0bb12b8e6557b616c0ae99bf284ae0d9732
Joseph Aiken <josephaiken92@gmail.com>
2018-01-09 15:06:24 +0000

    small change to server

--- server.js
+++ server.js
@@ -32,7 +32,10 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/phishScrapeDB");
+mongoose.connect(MONGODB_URI, {
+
+    useMongoClient: true
+});
 
 // Routes
 
@@ -84,6 +87,22 @@ app.get("/articles", function(req, res) {
             res.json(err);
         });
 });
+
+// Route for getting all Articles from the db
+app.get("/savedArticles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article
+        .find({})
+        .then(function(dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
