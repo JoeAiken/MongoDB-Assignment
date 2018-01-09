4b7dee766fa9c94dad7a3a43d6889ce25c509293
Joseph Aiken <josephaiken92@gmail.com>
2018-01-09 02:15:08 +0000

    app which works exactly like exercise 20 from class

---
+++ Article.js
@@ -0,0 +1,7 @@
+MongoDB shell version: 2.6.12
+connecting to: test
+Server has startup warnings: 
+2018-01-08T23:23:01.992+0000 ** WARNING: --rest is specified without --httpinterface,
+2018-01-08T23:23:01.992+0000 **          enabling http interface
+> [3G[Jdb[5G[3G[J[3G[3G[J[3G^C
+bye
